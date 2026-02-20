bdb5aed861007ce03539999b2ac39ea3c02a8a17
Idreesh Khan <idreesh@Idreeshs-MacBook-Air.local>
2026-02-20 00:47:42 +0000

    Finished Exception Handling and Try Catch methods

---
+++ src/main/java/com/example/oppsconcept/ExceptionHandling/TryWithReaderTest.java
@@ -0,0 +1,18 @@
+package com.example.oppsconcept.ExceptionHandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithReaderTest {
+    public static void main(String[] args) {
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("example.txt"));
+            String line;
+            while((line = reader.readLine()) != null);
+            System.out.println(line);
+        } catch ( IOException e){
+            System.out.println("IOEXCEPTIONCAUGHT" + e.getMessage());
+        }
+    }
+}
